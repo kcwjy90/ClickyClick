5abd7868f6d6b998f3fcc914422a6aecc7fbc49e
kcwjy90 <kcwjy90@gmail.com>
2019-01-10 18:28:09 -0800

    Updates

---
+++ precache-manifest.9742423d4e8682d096ef2edaeea9280b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f386db17176df1eee566a358040e2754",
+    "url": "/ClickyClick/static/media/bk2.f386db17.jpg"
+  },
+  {
+    "revision": "a910d799b7c2ad7f96a6",
+    "url": "/ClickyClick/static/js/runtime~main.a910d799.js"
+  },
+  {
+    "revision": "615540ea2cc14e747d4e",
+    "url": "/ClickyClick/static/js/main.615540ea.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyClick/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "615540ea2cc14e747d4e",
+    "url": "/ClickyClick/static/css/main.2ce3cf41.chunk.css"
+  },
+  {
+    "revision": "f0bded6d86d85e996ebcd64b576e599c",
+    "url": "/ClickyClick/index.html"
+  }
+];
